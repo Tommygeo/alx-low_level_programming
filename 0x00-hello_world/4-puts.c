d75fda09aef12df1c52f5051b4e180fc79b9744a
Tommygeo <georgeolotu28@gmail.com>
2022-09-08 20:33:26 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,15 +7,10 @@
  * main - print in the put function
  *
  * Return: Always 0 (It is successful)
- *
  */
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }
