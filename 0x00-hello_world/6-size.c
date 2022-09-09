9953cf95b0e89b791f6d97da7d4ad26ff5e802bf
Tommygeo <georgeolotu28@gmail.com>
2022-09-08 21:04:04 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the string withe printf function
+ *
+ * Return: Always 0 (this is successful)
+ * /
+ int main(void)
+ {
+          char a;
+	  int b;
+	  long int c
+	  float e;
+
+	  printf("Size of a char: %ld byte(s)\n", sizeof(a));
+	  printf("Size of an int: %ld byte(s)\n", sizeof(b));
+	  printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+	  printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
+	  printf("Size of a float: %ld byte(s)\n", sizeof(e));
+	  return (0);
+ } 
