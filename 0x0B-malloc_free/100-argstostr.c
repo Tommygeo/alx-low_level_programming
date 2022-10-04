5952aadd3a4e1a19d7d1ea3a28f67c936bd06f30
Tommygeo <georgeolotu28@gmail.com>
2022-10-04 10:09:53 -0700

    TAsk 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,20 +3,20 @@
 
 /**
  * argstostr - concatenates all the arguments of your program
- * @ac: number of arguments
- * @av: double pointer to arguments
- * Return: pointer to new string, or NULL if error
+ * @ac: number of the arguments
+ * @av: double pointer to the arguments
+ * Return: pointer to new string or NULL if error
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k = 0, n = 0;
+	int a, b, c = 0, n = 0;
 	char *s;
 
 	if (ac <= 0 || av == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
+	for (a = 0; a < ac; a++)
 	{
-		for (j = 0; av[i][j]; j++)
+		for (b = 0; av[a][b]; b++)
 			n++;
 		n++;
 	}
@@ -24,16 +24,16 @@ char *argstostr(int ac, char **av)
 	s = malloc(n * sizeof(char));
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
+	for (a = 0; a < ac; a++)
 	{
-		for (j = 0; av[i][j]; j++)
+		for (b = 0; av[a][b]; b++)
 		{
-			s[k] = av[i][j];
-			k++;
+			s[c] = av[a][b];
+			c++;
 		}
-		s[k] = '\n';
-		k++;
+		s[c] = '\n';
+		c++;
 	}
-	s[k] = '\0';
+	s[c] = '\0';
 	return (s);
 }
